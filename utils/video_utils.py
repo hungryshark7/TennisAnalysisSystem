04435a31f85a4491197b460e3feec9ce11680535
hungryshark7 <barakaelilawuo0101@gmail.com>
2025-09-14 23:28:00 -0400

    video utils fixed

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -1,4 +1,4 @@
-import cv2 
+import cv2
 
 def read_video(video_path):
     cap = cv2.VideoCapture(video_path)
@@ -11,12 +11,9 @@ def read_video(video_path):
     cap.release()
     return frames
 
-
 def save_video(output_video_frames, output_video_path):
-    fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-    fps = 30
-    height, width, _ = output_video_frames[0].shape
-    out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    out = cv2.VideoWriter(output_video_path, fourcc, 24, (output_video_frames[0].shape[1], output_video_frames[0].shape[0]))
     for frame in output_video_frames:
         out.write(frame)
     out.release()
